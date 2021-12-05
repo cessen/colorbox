fa7a65d2092fcccf7fe299dd5c494661a1f19b87
Nathan Vegdahl <cessen@cessen.com>
2021-12-05 11:16:28 -0800

    Add chromaticities of more color spaces.

--- src/chroma.rs
+++ src/chroma.rs
@@ -30,7 +30,15 @@ pub const REC2020: Chromaticities = Chromaticities {
     w: (0.3127, 0.3290),
 };
 
-/// The ACES AP0 chromaticities.
+/// DCI-P3 chromaticities.
+pub const DCI_P3: Chromaticities = Chromaticities {
+    r: (0.680, 0.320),
+    g: (0.265, 0.690),
+    b: (0.150, 0.060),
+    w: (0.314, 0.351),
+};
+
+/// ACES AP0 chromaticities.
 ///
 /// These are the chromaticities of the ACES2065-1 color space.
 pub const ACES_AP0: Chromaticities = Chromaticities {
@@ -40,7 +48,7 @@ pub const ACES_AP0: Chromaticities = Chromaticities {
     w: (0.32168, 0.33767),
 };
 
-/// The ACES AP1 chromaticities.
+/// ACES AP1 chromaticities.
 ///
 /// These are the chromaticities of e.g. the ACEScg, ACEScc, and
 /// ACEScct color spaces.
@@ -49,4 +57,28 @@ pub const ACES_AP1: Chromaticities = Chromaticities {
     g: (0.165, 0.830),
     b: (0.128, 0.044),
     w: (0.32168, 0.33767),
+};
+
+/// Adobe RGB chromaticities.
+pub const ADOBE_RGB: Chromaticities = Chromaticities {
+    r: (0.6400, 0.3300),
+    g: (0.2100, 0.7100),
+    b: (0.1500, 0.0600),
+    w: (0.3127, 0.3290),
+};
+
+/// Adobe Wide-gamut RGB chromaticities.
+pub const ADOBE_WIDE_GAMUT_RGB: Chromaticities = Chromaticities {
+    r: (0.7347, 0.2653),
+    g: (0.1152, 0.8264),
+    b: (0.1566, 0.0177),
+    w: (0.3457, 0.3585),
+};
+
+/// Kodak ProPhoto RGB chromaticities.
+pub const PROPHOTO: Chromaticities = Chromaticities {
+    r: (0.734699, 0.265301),
+    g: (0.159597, 0.840403),
+    b: (0.036598, 0.000105),
+    w: (0.345704, 0.358540),
 };
